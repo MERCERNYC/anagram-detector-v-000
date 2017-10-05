8061c3cba4d4a3bf12247adc2c44173f2f03572a
Juliana Fogg <julianafogg@gmail.com>
2017-10-05 15:33:54 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,13 @@ class Anagram
     @anagram = anagram
  end
 
- def match(words)
+ def match(words) #detect
     words.select do |word|
-      is_anagram?(word)
+      is_anagram?(word)#true or false
     end
-  end 
+  end
 
- def is_anagram?(word)
+ def is_anagram?(word) #should return an empty array used chars.sort to keep the same order
    word.chars.sort = @anagram.chars.sort
     end 
-  end 
-
+  end
